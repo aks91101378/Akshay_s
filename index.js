9d77ad110b7b09db7876a6cf1ceb3cdf0be8b3d4
aks91101378 <aks91101378@gmail.com>
2024-12-18 23:55:12 +0530

    good

---
+++ index.js
@@ -0,0 +1,18 @@
+let loginForm = document.querySelector('.header .login-form');
+let contactInfo = document.querySelector('.contact-info');
+
+
+
+document.querySelector('#login-btn').onclick = () =>{
+   loginForm.classList.toggle('active');
+   navbar.classList.remove('active');
+   searchForm.classList.remove('active'); 
+};
+
+document.querySelector('#info-btn').onclick = () =>{
+   contactInfo.classList.add('active');
+}
+
+document.querySelector('#close-contact-info').onclick = () =>{
+   contactInfo.classList.remove('active');
+}
